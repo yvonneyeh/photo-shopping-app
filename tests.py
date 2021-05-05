d20be97a4ee6fd6b3812ba9762719fef767cc0ff
Yvonne Yeh <code@yvonneyeh.com>
2021-05-05 16:55:54 -0700

    Add tests for Flask app

---
+++ tests.py
@@ -0,0 +1,40 @@
+import os
+import unittest
+import pytest
+from config import db
+from server import app
+from flaskr import flaskr
+
+
+@pytest.fixture
+def client():
+    db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+    flaskr.app.config['TESTING'] = True
+
+    with flaskr.app.test_client() as client:
+        with flaskr.app.app_context():
+            flaskr.init_db()
+        yield client
+
+    os.close(db_fd)
+    os.unlink(flaskr.app.config['DATABASE'])
+
+
+def login(client, username, password):
+    return client.post('/login', data=dict(
+        username=username,
+        password=password
+    ), follow_redirects=True)
+
+
+def logout(client):
+    return client.get('/logout', follow_redirects=True)
+
+
+# class FlaskTests(unittest.TestCase):
+
+# class TestPhotoFunctions(unittest.TestCase):
+
+
+if __name__ == '__main__':
+    unittest.main()
